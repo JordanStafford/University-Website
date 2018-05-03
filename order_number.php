8fcedf58adbcb1c105344f8cac3e66a739f15edf
Stafford <jordan.stafford@sage016926.sageukie.adinternal.com>
2018-05-03 13:19:34 +0100

    Moved CSS

--- order_number.php
+++ order_number.php
@@ -6,46 +6,6 @@
   <link rel="stylesheet" href="order_number.css" />
   <link href='https://fonts.googleapis.com/css?family=Raleway' />
   <style>
-  body {
-    font-size: 16px;
-  }
-  ul {
-    list-style-type: none;
-    margin: 0;
-    padding: 0;
-    overflow: hidden;
-    background-color: #666666;
-    position: -webkit-sticky; /*safari*/
-    position: sticky;
-    top: 0;
-  }
-  li {
-    float: left;
-  }
-  li a {
-    display: block;
-    color: white;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-  }
-  li a:hover {
-    background-color:  #2E8B57;
-  }
-  .active {
-    background-color: #2E8B57;
-  }
-  * {
-    box-sizing: border-box;
-  }
-  .row{
-    display: flex;
-  }
-  .column {
-    flex: 50px;
-    padding: 10px;
-    height: 300px;
-  }
   </style>
 </head>
 <body>
