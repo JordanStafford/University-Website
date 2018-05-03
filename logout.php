0c9f24e87f4d82fb55d633a0eab448e318e9a544
Stafford <jordan.stafford@sage016926.sageukie.adinternal.com>
2018-05-03 13:32:16 +0100

    Moved CSS

--- logout.php
+++ logout.php
@@ -6,30 +6,6 @@ session_start();
     <link rel="stylesheet" href="logout.css" />
     <link href='https://fonts.googleapis.com/css?family=Raleway' />
   <style>
-  .active {
-    background-color: #2E8B57;
-  }
-  .button {
-    display: inline-block;
-    padding: 15px 25px;
-    font-size: 24px;
-    cursor: pointer;
-    text-align: center;
-    text-decoration: none;
-    outline: #fff;
-    background-color: #2E8B57;
-    border: none;
-    border-radius: 15px;
-    box-shadow: 0 9px #999;
-    color: white;
-  }
-  .button:hover {background-color: #3CB371}
-
-  .button:active{
-    background-color: #3e8e41;
-    box-shadow: 0 5px #666;
-    transform: translateY(4px);
-  }
   </style>
 </head>
 <body>
